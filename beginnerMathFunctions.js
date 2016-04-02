d7d78074a908fc40d51e14f2adcc632bfdf38142
Alex J. Gray <alexgray410@gmail.com>
2016-04-02 02:47:56 -0400

    Fixed Indentation

--- beginnerMathFunctions.js
+++ beginnerMathFunctions.js
@@ -6,24 +6,24 @@ var sum = function(x,y){
 var range = function(x, y, step){
 	if (step == null) step = 1;
   var arr = [];
+  
   if (step > 0) {
-    	for (var i = x; i <= y; i += step)
+    for (var i = x; i <= y; i += step)
+      arr.push(i);
+  } else {
+    	for (var i = x; i >= y; i += step)
       	arr.push(i);
-  		} else {
-    		for (var i = x; i >= y; i += step)
-      		arr.push(i);
-    	}
- 		 	return arr;
-			};
+  }
+ 	return arr;
+};
 // rangeSum(start,end) gives the sum of the range from start to end.          
 var rangeSum = function(start,end){
   var arr = [];
-  for (var i = start; i <= end; i++) {
-       		arr.push(i);
-          }
-          return arr.reduce(function(a,b){
-               	return a + b;
-          });
+  for (var i = start; i <= end; i++) 
+    arr.push(i);
+  return arr.reduce(function(a,b){
+    return a + b;
+  });
 };
 
 
