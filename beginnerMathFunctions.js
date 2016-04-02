c7e70a382caaef24b5d498b08956fc9c26afd13f
Alex J. Gray <alexgray410@gmail.com>
2016-04-02 18:44:14 -0400

    Improved range function.

--- beginnerMathFunctions.js
+++ beginnerMathFunctions.js
@@ -1,4 +1,4 @@
-// adder(base) takes the sum of the arguments passed to it if they are numbers.
+// adder(base) takes the sum of the arguments passed to it if they are numbers. Returns NaN otherwise.
 function adder(base) {
   base = Number(base);
   for (var i = 1; i < arguments.length; i++) {
@@ -7,19 +7,30 @@ function adder(base) {
   return base;
 }
 // range(x,y,step) gives an array of the range of x and y, with an optional step argument
-var range = function(x, y, step){
-	if (step == null) step = 1;
-  var arr = [];
-  
-  if (step > 0) {
-    for (var i = x; i <= y; i += step)
-      arr.push(i);
-  } else {
-    	for (var i = x; i >= y; i += step)
-      	arr.push(i);
+// Detects if start > end and lists appropriately 
+var range = function(start, end, step){
+  start = Number(start);
+  end = Number(end);
+  if (step == undefined) {step = 1;}
+  step = Math.abs(step);
+  var result = [];
+  if (step == 0){
+    return start;
   }
- 	return arr;
+  else if (start > end){
+    for (var i = start; i >= end; i -= step){
+      result.push (i);
+    }
+    return result;
+    } else {
+      for (var i = start; i <= end; i += step){
+        result.push (i);
+      }
+  return result;
+  };
 };
+
+
 // rangeSum(start,end) gives the sum of the range from start to end.          
 var rangeSum = function(start,end){
   var arr = [];
